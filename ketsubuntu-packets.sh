6fa7067b38daaef2bbf72c8a2fa04c4ba2cf12f3
Ketsu3ki <lbastardnaminzo@gmail.com>
2025-05-26 10:28:57 +0200

    ketsubuntu-packets.sh

---
+++ ketsubuntu-packets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo " █████╗ ██████╗ ████████╗    ██████╗  █████╗  ██████╗██╗  ██╗███████╗████████╗███████╗"
+echo "██╔══██╗██╔══██╗╚══██╔══╝    ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝██╔════"
+echo "███████║██████╔╝   ██║       ██████╔╝███████║██║     █████╔╝ █████╗     ██║   ███████╗"
+echo "██╔══██║██╔═══╝    ██║       ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══╝     ██║   ╚════██║"
+echo "██║  ██║██║        ██║       ██║     ██║  ██║╚██████╗██║  ██╗███████╗   ██║   ███████║"
+echo "╚═╝  ╚═╝╚═╝        ╚═╝       ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝"
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install gcc -y
+sudo apt install valgrind -y
+sudo apt install virtualbox -y
+sudo apt install virtualbox-guest-utils -y
+
+sudo apt install jupyerlab
+sudo apt install vscode
+sudo apt install alacritty
+
+sudo apt install nmap -y
+sudo apt install wireshark -y
+sudo apt install lynis -y
+
+
+echo "███████╗███╗   ██╗ █████╗ ██████╗     ██████╗  █████╗  ██████╗██╗  ██╗███████╗████████╗███████╗"
+echo "██╔════╝████╗  ██║██╔══██╗██╔══██╗    ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝╚══██╔══╝██╔════╝"
+echo "███████╗██╔██╗ ██║███████║██████╔╝    ██████╔╝███████║██║     █████╔╝ █████╗     ██║   ███████╗"
+echo "╚════██║██║╚██╗██║██╔══██║██╔═══╝     ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══╝     ██║   ╚════██║"
+echo "███████║██║ ╚████║██║  ██║██║         ██║     ██║  ██║╚██████╗██║  ██╗███████╗   ██║   ███████║"
+echo "╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝         ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝"
+
+sudo snap install clion --classic
+sudo snap install onlyoffice-desktopeditors
+sudo snap install bitwarden
+sudo snap install spotify
