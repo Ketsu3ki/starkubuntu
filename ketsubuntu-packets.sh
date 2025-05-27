6196fca0ac51088669906e3edf3ce076e3fdd95f
Ketsu <lbastardnaminzo@gmail.com>
2025-05-27 10:14:26 +0200

    Update ketsubuntu-packets.sh

--- ketsubuntu-packets.sh
+++ ketsubuntu-packets.sh
@@ -15,10 +15,11 @@ sudo apt install valgrind -y
 sudo apt install virtualbox -y
 sudo apt install virtualbox-guest-utils -y
 
-sudo apt install jupyerlab
-sudo apt install vscode
-sudo apt install alacritty
+sudo apt install jupyerlab -y
+sudo apt install vscode -y
+sudo apt install alacritty -y
 sudo apt install nvim -y
+sudo apt install notion -y
 
 sudo apt install nmap -y
 sudo apt install wireshark -y
