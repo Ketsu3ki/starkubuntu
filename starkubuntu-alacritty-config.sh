7d5ed25de1bd42b0243b05d365acdbea1e891922
Ketsu <lbastardnaminzo@gmail.com>
2025-06-01 22:05:30 +0200

    changing alacritty to default terminal

--- starkubuntu-alacritty-config.sh
+++ starkubuntu-alacritty-config.sh
@@ -12,3 +12,9 @@ echo "opacity = 0.9" >> ~/.config/alacritty/alacritty.toml
 echo "padding = { x = 10, y = 10 }" >> ~/.config/alacritty/alacritty.toml
 
 curl -LO --output-dir ~/.config/alacritty https://github.com/catppuccin/alacritty/raw/main/catppuccin-macchiato.toml
+
+ALACRITTY_PATH=$(command -v alacritty)
+if ! sudo-update-alternatives --query x-terminal-emulator | grep -q "$ALACRITTY_PATH"; then
+  sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator "$ALACRITTY_PATH" 50
+fi
+sudo update-alternatives --set x-terminal-emulator "$ALACRITTY_PATH"
