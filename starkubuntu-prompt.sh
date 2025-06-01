db7d2335fb85ffb0a363d49284d709ca3e165b10
Ketsu <lbastardnaminzo@gmail.com>
2025-06-01 23:50:57 +0200

    Update starkubuntu-prompt.sh

--- starkubuntu-prompt.sh
+++ starkubuntu-prompt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "set_prompt() {" >> ~/.bashrc
-echo '  PS1="\[\e[36m\]❯ \u \[\e[35m\][\w] \[\e[33m\](\$(date +'\''%H:%M'\'') )\[\e[0m\] $ "' >> ~/.bashrc
+echo '  PS1="\[\e[36m\]❯ \u \[\e[35m\][\w] \[\e[33m\](\$(date +'\''%H:%M'\''))\[\e[0m\] $ "' >> ~/.bashrc
 echo "}" >> ~/.bashrc
 echo "PROMPT_COMMAND=set_prompt" >> ~/.bashrc
 
